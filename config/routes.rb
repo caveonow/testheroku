58bb594cf3eec41e67fb772acb77d40755c230c0
caveonow <caveon_5759@hotmail.com>
2016-10-09 11:39:48 +0800

    set root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :orders
-  root :to => "order#index"
+  root :to => "orders#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
